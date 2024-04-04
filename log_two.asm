05100538b33fa51b7396dba56f7ace0b8addce7b
Joshua Vink <joshuavink34@gmail.com>
2024-04-04 09:57:04 -0400

    Add files via upload

---
+++ log_two.asm
@@ -0,0 +1,28 @@
+.globl log_two
+.text
+	addi $a0, $0, -5 # x
+	
+	blt $a0, 0, negative
+log_two:
+	ble $a0, 1, finish
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+	sw $a0, 4($sp)
+	
+	sra $a0, $a0, 1
+	jal log_two
+	addi $v0, $v0, 1
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 8
+	jr $ra
+	
+
+finish:
+	addi, $v0, $0, 0
+	jr $ra
+	
+negative:
+	addi, $v0, $0, -1
+	jr $ra
+	
